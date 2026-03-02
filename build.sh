141ce41488b63324a48d6a2d16ad8b8e8b9c8c7b
Levi <liwei-8466@qq.com>
2026-03-02 09:30:00 +0800

    Simplify build process in build.sh
    Removed unnecessary build steps for documentation and OpenAPI.

--- build.sh
+++ build.sh
@@ -10,14 +10,3 @@ cd .gopeed-js
 rm -rf .git
 pnpm install
 pnpm run build
-
-# Build gopeed-js API Reference Document
-pnpm run docs
-# Copy dist to /docs/public/site/reference
-cp -r docs/* ../.vitepress/dist/site/reference/
-
-# Build gopeed-js OpenAPI sepc
-cd packages/gopeed-openapi
-pnpm run build
-# Copy swagger.json to /docs/public/site/openapi
-cp build/swagger.json ../../../.vitepress/dist/site/openapi/
